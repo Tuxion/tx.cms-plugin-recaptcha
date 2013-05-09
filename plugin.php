1268d7a7d6e29819bf4ab9c2ffee759065788095
Beanow <beanow@project-virality.com>
2013-05-09 18:14:18 +0200

    Didn't need the Ajax library.

--- plugin.php
+++ plugin.php
@@ -1,6 +1,2 @@
 <?php namespace plugins; if(!TX) die('No direct access.');
 require_once("src/recaptchalib.php");
-
-tx('Ob')->script('recaptcha_ajax');
-  echo t.t.'<script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>'.n;
-tx('Ob')->end();
